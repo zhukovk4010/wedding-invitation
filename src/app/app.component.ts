763c41b4fd817d8d85f8a198c737d4673bde068e
Zhukov Kirill PC <zhukovk4010@gmail.com>
2025-01-25 15:59:54 +0300

    add desktop overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DesktopOverlayComponent } from '../components/desktop-overlay/desktop-overlay.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DesktopOverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wedding-invitation';
+  public windowWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number) {
+    this.windowWidth.set(width);
+  }
 }
---
+++ src/components/desktop-overlay/desktop-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-overlay',
+  imports: [],
+  templateUrl: './desktop-overlay.component.html',
+  styleUrl: './desktop-overlay.component.scss'
+})
+export class DesktopOverlayComponent {
+
+}
