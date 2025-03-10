a3c5fbe664da735648f0366abb7ddb3cba5c3676
Zhukov Kirill PC <zhukovk4010@gmail.com>
2025-03-10 18:45:46 +0300

    add 2 pages

--- src/pages/yeskov/yeskov.component.ts
+++ src/pages/yeskov/yeskov.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {usersData} from '../../data/usersData';
+import {MeetingScreenComponent} from '../../components/meeting-screen/meeting-screen.component';
+import {WelcomeImgSectionComponent} from '../../components/welcome-img-section/welcome-img-section.component';
+import {WelcomeSectionComponent} from '../../components/welcome-section/welcome-section.component';
+import {TimingSectionComponent} from '../../components/timing-section/timing-section.component';
+import {PlaceSectionComponent} from '../../components/place-section/place-section.component';
+import {DressCodeSectionComponent} from '../../components/dress-code-section/dress-code-section.component';
+import {FaqSectionComponent} from '../../components/faq-section/faq-section.component';
+import {FormSectionComponent} from '../../components/form-section/form-section.component';
 
 @Component({
   selector: 'app-yeskov',
-  imports: [],
+  imports: [
+    MeetingScreenComponent,
+    WelcomeImgSectionComponent,
+    WelcomeSectionComponent,
+    TimingSectionComponent,
+    PlaceSectionComponent,
+    DressCodeSectionComponent,
+    FaqSectionComponent,
+    FormSectionComponent
+  ],
   templateUrl: './yeskov.component.html',
   styleUrl: './yeskov.component.scss'
 })
 export class YeskovComponent {
+  isMeetingScreenOpen = signal(true);
 
+  onUnlock() {
+    this.isMeetingScreenOpen.set(false);
+  }
+
+  protected readonly usersData = usersData;
 }
